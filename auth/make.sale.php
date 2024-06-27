43f96f2b101c753edfa27ed2b0ad481b192a19a5
morotijani <tjhackx111@gmail.com>
2024-06-27 20:28:21 +0000

    initial commit

--- auth/make.sale.php
+++ auth/make.sale.php
@@ -17,9 +17,25 @@ if (isset($_POST['gram-amount'])) {
 		$carat = calculateCarat($gram, $volume);
 		$total_amount = calculateTotalAmount($gram, $volume, $current_price = '12.99');
 
+		$sale_id = guidv4();
+		$createdAt = date("Y-m-d H:i:s A");;
+		$sql = "
+			INSERT INTO `jspence_sales`(`sale_id`, `sale_gram`, `sale_volume`, `sale_density`, `sale_pounds`, `sales_carat`, `sale_total_amount`, `sale_comment`, `sale_by`, `createdAt`) 
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		";
+		$statement = $conn->prepare($sql);
+		$result = $statement->execute([$sale_id, $gram, $volume, $density, $pounds, $carat, $total_amount, $note, 1, $createdAt]);
+		if (isset($result)) {
+			// code...
+			$message = "added new sale with gram of " . $gram . " and volume of " . $volume . " and total amount of " . $total_amount ." ";
+			$log_admin = 1;
+			add_to_log($message, $log_admin);
 
+			echo "";
+		} else {
+			echo 'Something went wrong.';
+		}
 
-		echo '';
 	} else {
 		echo "Your PIN is invalid!";
 	}
--- helpers/functions.php
+++ helpers/functions.php
@@ -35,3 +35,20 @@ function calculateTotalAmount($gram, $volume, $current_price) {
 function round_to_decimal_place($decimal_place, $figure) {
 	return number_format((float)$figure, $decimal_place, '.', '');
 }
+
+function add_to_log($message, $log_admin) {
+	global $conn;
+
+	$log_id = guidv4();
+	$createdAt = date("Y-m-d H:i:s A");
+	$sql = "
+		INSERT INTO `jspence_logs`(`log_id`, `log_message`, `log_admin`, `createdAt`) 
+		VALUES (?, ?, ?, ?)
+	";
+	$satement = $conn->prepare($sql);
+	$result = $statement->execute([$log_id, $message, $log_admin, $createdAt]);
+
+	if ($result) {
+		return true;
+	}
+}
