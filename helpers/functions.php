947d4ef1adcf5bee19345069e469479d10e5f669
morotijani <tjhackx111@gmail.com>
2024-06-28 14:56:38 +0000

    initial commit

--- helpers/functions.php
+++ helpers/functions.php
@@ -83,13 +83,64 @@ function fetch_all_sales($status) {
 	                <td>' . money($row["sale_total_amount"]) . '</td>
 	                <td>' . pretty_date($row["createdAt"]) . '</td>
 	                <td class="text-end">
-	                    <button type="button" class="btn btn-sm btn-square btn-neutral w-rem-6 h-rem-6" title="More">
+	                    <button type="button" class="btn btn-sm btn-square btn-neutral w-rem-6 h-rem-6" title="More" data-bs-target="#saleModal_' . $row["id"] . '" data-bs-toggle="modal">
 	                        <i class="bi bi-three-dots"></i>
 	                    </button> <button type="button" title="Print receipt" class="btn btn-sm btn-square btn-neutral w-rem-6 h-rem-6">
 	                        <i class="bi bi-receipt"></i>
 	                    </button>
 	                </td>
 	            </tr>
+
+	            <div class="modal fade" id="saleModal_' . $row["id"] . '" tabindex="-1" aria-labelledby="saleModalLabel_' . $row["id"] . '" aria-hidden="true">
+					<div class="modal-dialog modal-dialog-centered">
+						<div class="modal-content overflow-hidden">
+							<div class="modal-header pb-0 border-0">
+								<h1 class="modal-title h4" id="saleModalLabel_' . $row["id"] . '">Select token</h1>
+								<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+							</div>
+							<div class="modal-body p-0">
+								<ul class="list-group">
+									<li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Total amount,</small>
+				                        <p>` + $("#total-amount").val() + ` ₵</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Gram</small>
+				                        <p>` + Number($("#gram-amount").val()).toFixed(2) + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Volume</small>
+				                        <p>` + Number($("#volume-amount").val()).toFixed(2) + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Density</small>
+				                        <p>` + $("#density").text() + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Pounds</small>
+				                        <p>` + $("#pounds").text() + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Carat</small>
+				                        <p id="send-amount">` + $("#carat").text() + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Customer</small>
+				                        <p id="send-amount">Name: ` + $("#customer_name").val() + ` | Contact: ` + $("#customer_contact").val() + `</p>
+				                    </li>
+				                    <li class="list-group-item" style="padding: 0.1rem 1rem;">
+				                        <small class="text-muted">Note</small>
+				                        <p>` + $("#note").val() + `</p>
+				                    </li>
+								</ul>
+								<div class="p-2"></div>
+								<div class="px-6 py-5 bg-body-secondary d-flex justify-content-center">
+									<button class="btn btn-sm btn-dark"><i class="bi bi-gear me-2"></i>Manage tokens</button>&nbsp;<button class="btn btn-sm btn-dark"><i class="bi bi-receipt me-2"></i>Print receipt</button>&nbsp;<button class="btn btn-sm btn-dark"><i class="bi bi-trash3 me-2"></i>Delete</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			';
 			$i++;
 		}
