b9a90c1e65a2bcdae8c977a5ae4c31ac7efc7d01
morotijani <tjhackx111@gmail.com>
2024-07-07 20:22:56 +0100

    initial commit

--- config.php
+++ config.php
@@ -4,19 +4,7 @@
 	
 	define('PROOT', '/jspence/');
 
-	define('TAXRATE', '2');
 	define('FIXED_POUNDS_FIGURE', '7.75');
 	define('FIXED_CARAT_FIGURE_1', '10.51');
 	define('FIXED_CARAT_FIGURE_2', '52.838');
 	define('FIXED_TOTAL_FIGURE', '23');
-
-	define('RECAPTCHA_SITE_KEY_PUBLIC', '6LejwV0jAAAAAAqPOSLq6mRMRZNnK5di3PQ3iRRj');
-	define('RECAPTCHA_SITE_KEY_SECRETE', '6LejwV0jAAAAAMpUdo_o-AcE_-5ciCZXJrBg3ATk');
-
-	define('RECAPTCHA_KEY', '6Lcx1lMjAAAAAPzgKTIkoH30gNPQ9-xVe49ssSLC');
-
-	define('MAIL_MAIL', 'info@garypie.com');
-
-	define('MAIL_KEY', 'Hn4f44da2');
-
-	define('IPINFO_KEY', 'c5c08603163207');
