2b8e50abe37c0dd915c1abc1b9ec0413e75a6d6a
morotijani <tjhackx111@gmail.com>
2024-09-13 23:46:33 +0000

    initial commiy

--- auth/analytics.info.php
+++ auth/analytics.info.php
@@ -1,15 +1,64 @@
 <?php 
 
-	// DELETE admin profile picture
+// Analytics info 
 
-	require_once ("../db_connection/conn.php");
+require_once ("../db_connection/conn.php");
 
-	if (isset($_POST['dater'])) {
-        $date = sanitize($_POST['dater']);
+if (isset($_POST['dater'])) {
+    $dater = sanitize($_POST['dater']);
 
-        if ($date == null) {
-            echo null;
-        } else {
-            echo $date;
-        }
+    $andDaily = '';
+    $andExpenditure = '';
+    $and = '';
+    if ($date != null) {
+        $and = ' AND CAST(jspence_sales.createdAt AS date) = "'.$dater.'"';
+        $andDaily = ' AND daily_date = "'.$dater.'"';
+        $andExpenditure = ' AND CAST(jspence_expenditures.createdAt AS date) = "'.$dater.'"';
     }
+
+    $sql = "
+        SELECT SUM(daily_capital) AS capital, SUM(daily_balance) AS balance 
+        FROM jspence_daily 
+        WHERE status = ? 
+        $andDaily
+    ";
+    $statement = $conn->prepare($sql);
+    $result = $statement->execute([0]);
+    $rows = $statement->fetchAll();
+    $row = $rows[0];
+
+    $ins = $conn->query("SELECT SUM(sale_total_amount) AS ins_amt, CAST(jspence_sales.createdAt AS date) AS in_d FROM jspence_sales WHERE sale_type = 'in' AND sale_status = 0 $and")->fetchAll();
+    $outs = $conn->query("SELECT SUM(sale_total_amount) AS outs_amt, CAST(jspence_sales.createdAt AS date) AS out_d FROM jspence_sales WHERE sale_type = 'out' AND sale_status = 0 $and")->fetchAll();
+    $expense = $conn->query("SELECT SUM(expenditure_amount) AS exp_amt, CAST(jspence_expenditures.createdAt AS date) AS exp_d FROM jspence_expenditures WHERE status = 0 $andExpenditure")->fetchAll();
+    $count_trades = $conn->query("SELECT *, CAST(jspence_sales.createdAt AS date) AS c_d FROM jspence_sales WHERE sale_status = 0 $and")->rowCount();
+
+    $gained_or_loss = 0;
+    $in = (($ins[0]['ins_amt']) ? $ins[0]['ins_amt'] : 0);
+    $out = (($outs[0]['outs_amt']) ? $outs[0]['outs_amt'] : 0);
+    $expenses = (($expense[0]['exp_amt']) ? $expense[0]['exp_amt'] : 0);
+
+    $output = [
+        'capital' => 0,
+        'balance' => 0,
+        'gained_or_loss' => 0,
+        'in' => $in,
+        'out' => $out,
+        'trades' => $count_trades,
+        'expenses' => $expenses
+    ];
+    if ($statement->rowCount() > 0) {
+        $a = (float)($row['balance'] + $expenses);
+        $gained_or_loss = (float)($row['capital'] - $a);
+    }
+
+    $output = [
+        'capital' => $row['capital'],
+        'balance' => $row['balance'],
+        'gained_or_loss' => $gained_or_loss,
+        'in' => $in,
+        'out' => $out,
+        'trades' => $count_trades,
+        'expenses' => $expenses
+    ];
+
+}
