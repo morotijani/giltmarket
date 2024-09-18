d2ba2f82f344f9504f8ea18d6420f2cea41c0d24
morotijani <tjhackx111@gmail.com>
2024-09-18 21:25:58 +0000

    initial commit

--- auth/login.php
+++ auth/login.php
@@ -34,3 +34,68 @@
         }
         
     }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
+    
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/favicon/favicon.ico" type="image/x-icon" />
+    
+    <!-- Fonts and icons -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
+    
+    <!-- Libs CSS -->
+    <link rel="stylesheet" href="./assets/css/libs.bundle.css" />
+    
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="./assets/css/theme.bundle.css" />
+    
+    <!-- Title -->
+    <title>Dashbrd</title>
+  </head>
+
+  <body class="d-flex align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12" style="max-width: 25rem">
+          <!-- Heading -->
+          <h1 class="fs-1 text-center">Sign in</h1>
+
+          <!-- Subheading -->
+          <p class="lead text-center text-body-secondary">Access our dashboard and start tracking your tasks.</p>
+
+          <!-- Form -->
+          <form class="mb-5">
+            <div class="mb-4">
+              <label class="visually-hidden" for="email">Email Address</label>
+              <input class="form-control" id="email" type="email" placeholder="Enter your email address..." />
+            </div>
+            <button class="btn btn-secondary w-100" type="submit">Sign in</button>
+          </form>
+
+          <!-- Text -->
+          <p class="text-center text-body-secondary mb-0">Don't have an account yet? <a href="./sign-up.html">Sign up</a>.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <!-- Map JS -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
+    
+    <!-- Vendor JS -->
+    <script src="./assets/js/vendor.bundle.js"></script>
+    
+    <!-- Theme JS -->
+    <script src="./assets/js/theme.bundle.js"></script>
+  </body>
+</html>
