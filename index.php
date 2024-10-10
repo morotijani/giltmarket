403dddfb062e044a4036244488b66a16c99f4683
morotijani <tjhackx111@gmail.com>
2024-10-10 11:37:27 +0000

    initial commit

--- index.php
+++ index.php
@@ -130,13 +130,34 @@
 							<div class="col-auto">
 								<!-- Avatar -->
 								<div class="avatar avatar-lg bg-body text-warning">
-								<i class="fs-4" data-duoicon="clock"></i>
+								<i class="fs-4" data-duoicon="align-bottom"></i>
 								</div>
 							</div>
 							</div>
 						</div>
 					</div>
           		</div>
+				  <div class="col-12 col-md-6 col-xxl-3">
+					<div class="card bg-body-tertiary border-transparent">
+						<div class="card-body">
+							<div class="row align-items-center">
+								<div class="col">
+									<!-- Heading -->
+									<h4 class="fs-base fw-normal text-body-secondary mb-1"><?= (($admin_data['admin_permissions'] == 'salesperson') ? 'Gold' : 'Money'); ?> accumulated</h4>
+
+									<!-- Text -->
+									<div class="fs-5 fw-semibold"><?= total_amount_today($admin_id); ?></div>
+								</div>
+								<div class="col-auto">
+									<!-- Avatar -->
+									<div class="avatar avatar-lg bg-body text-warning">
+										<i class="fs-4" data-duoicon="briefcase"></i>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div class="col-12 col-md-6 col-xxl-3 mb-4 mb-md-0">
 					<div class="card bg-body-tertiary border-transparent">
 						<div class="card-body">
@@ -177,27 +198,6 @@
 										</div>
 									</div>
 								<?php endif; ?>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-md-6 col-xxl-3">
-					<div class="card bg-body-tertiary border-transparent">
-						<div class="card-body">
-							<div class="row align-items-center">
-								<div class="col">
-									<!-- Heading -->
-									<h4 class="fs-base fw-normal text-body-secondary mb-1">Accumulated amount</h4>
-
-									<!-- Text -->
-									<div class="fs-5 fw-semibold"><?= total_amount_today($admin_id); ?></div>
-								</div>
-								<div class="col-auto">
-									<!-- Avatar -->
-									<div class="avatar avatar-lg bg-body text-warning">
-										<i class="fs-4" data-duoicon="clipboard"></i>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
