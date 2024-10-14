a249b3c231123046e811fda365b3a4a120a5ccfa
morotijani <tjhackx111@gmail.com>
2024-10-14 02:27:58 +0000

    initial commit

--- db_connection/jspence.sql
+++ db_connection/jspence.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 28, 2024 at 10:51 AM
+-- Generation Time: Oct 14, 2024 at 04:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -49,9 +49,9 @@ INSERT INTO `jspence` (`company_name`, `company_address`, `company_phone1`, `com
 
 CREATE TABLE `jspence_admin` (
   `id` int(11) NOT NULL,
-  `admin_id` varchar(300) NOT NULL,
+  `admin_id` varchar(100) NOT NULL,
   `admin_fullname` varchar(255) NOT NULL,
-  `admin_email` varchar(175) NOT NULL,
+  `admin_email` varchar(255) NOT NULL,
   `admin_phone` varchar(20) DEFAULT NULL,
   `admin_password` varchar(255) NOT NULL,
   `admin_pin` int(11) DEFAULT 1234,
@@ -67,12 +67,29 @@ CREATE TABLE `jspence_admin` (
 --
 
 INSERT INTO `jspence_admin` (`id`, `admin_id`, `admin_fullname`, `admin_email`, `admin_phone`, `admin_password`, `admin_pin`, `admin_profile`, `admin_joined_date`, `admin_last_login`, `admin_permissions`, `admin_status`) VALUES
-(1, '234234234', 'alhaji priest babson', 'admin@jspence.com', NULL, '$2y$10$dHoeddyBK4Z23jqePowDO.JPAeXDugtyZN6.Zwc8hy.033Z1/5vbq', 1234, 'assets/media/admin-profiles/971aaa4a3274e711f35f7201d56d19c9.png', '2020-02-21 21:01:31', '2024-09-15 22:58:17', 'admin,salesperson,supervisor', 0),
+(1, 'c454b2bf-9b1e-409a-8b0d-d84ab82cf22d', 'alhaji priest babson', 'admin@jspence.com', NULL, '$2y$10$dHoeddyBK4Z23jqePowDO.JPAeXDugtyZN6.Zwc8hy.033Z1/5vbq', 1234, 'assets/media/admin-profiles/971aaa4a3274e711f35f7201d56d19c9.png', '2020-02-21 21:01:31', '2024-10-08 19:57:56', 'admin,salesperson,supervisor', 0),
 (11, '16acd24f-0ad7-42d9-a565-a8863f4a8fa2', 'tijani moro', 'tijani@jspence.com', NULL, '$2y$10$6VM4wWjd3Ts2snR4KDRS9On2bRxzXJ0V/TXplZHs0ZL93y.G/RqWu', 1234, NULL, '2024-06-28 05:48:12', '2024-09-15 22:49:51', 'salesperson', 1),
-(12, 'e01de4bc-10e7-47cc-b2df-c2e1bdd8997f', 'inuwa mohammed umar', 'inuwa@jspence.com', NULL, '$2y$10$7mg6BRD9UXqQL8wxUiCkQe5IqceroHPGvq8wMgiiTCpFEOYsUdcNq', 2222, NULL, '2024-06-28 05:49:20', '2024-07-05 06:28:04', 'salesperson', 0),
+(12, 'e01de4bc-10e7-47cc-b2df-c2e1bdd8997f', 'inuwa mohammed umar', 'inuwa@jspence.com', NULL, '$2y$10$7mg6BRD9UXqQL8wxUiCkQe5IqceroHPGvq8wMgiiTCpFEOYsUdcNq', 2222, NULL, '2024-06-28 05:49:20', '2024-10-13 23:40:10', 'salesperson', 0),
 (13, '404d51db-6533-4586-b8d5-17c27c2f0607', 'henry asamoah', 'henry@email.com', NULL, '$2y$10$.pYicI6NOTj8Rd8S878EB.Hn6uoxCQXkix7uJgXlvxx1eR8iV1dLq', 1234, NULL, '2024-07-01 14:21:26', '2024-09-10 15:12:58', 'salesperson', 0),
 (14, '59e29767-cc32-4b2b-9abf-8422e2e45dcd', 'Adiza husein', 'adiza@email.com', NULL, '$2y$10$cC84GJNvi4Tq/6gm.r.ft.G9YEZ267sz3JQ/B/b.Nl5Cz6Fa64z9S', 1234, NULL, '2024-07-01 22:33:16', NULL, 'admin,salesperson,supervisor', 0),
-(15, '986785d8-7b98-4747-a0b2-8b4f4b239e06', 'emmanuel atim', 'emma@jspence.com', NULL, '$2y$10$lwzmqYK9BHTWrHL0FNxoju1FCQQfOY78T8nb9kEeH0dTzvRCannvW', 1234, NULL, '2024-09-09 17:19:05', '2024-09-14 20:53:07', 'supervisor', 0);
+(15, '986785d8-7b98-4747-a0b2-8b4f4b239e06', 'emmanuel atim', 'emma@jspence.com', NULL, '$2y$10$lwzmqYK9BHTWrHL0FNxoju1FCQQfOY78T8nb9kEeH0dTzvRCannvW', 1234, NULL, '2024-09-09 17:19:05', '2024-10-13 23:38:23', 'supervisor', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jspence_coffers`
+--
+
+CREATE TABLE `jspence_coffers` (
+  `id` bigint(20) NOT NULL,
+  `coffers_id` varchar(100) DEFAULT NULL,
+  `coffers_amount` double(10,2) DEFAULT NULL,
+  `coffers_for` varchar(100) DEFAULT NULL,
+  `coffers_status` enum('receive','send') DEFAULT NULL,
+  `coffers_receive_through` enum('cash','trades') DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -152,14 +169,6 @@ CREATE TABLE `jspence_logs` (
   `log_status` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `jspence_logs`
---
-
-INSERT INTO `jspence_logs` (`id`, `log_id`, `log_message`, `log_admin`, `createdAt`, `updatedAt`, `log_status`) VALUES
-(1, 'a6ba6471-188c-484c-908f-34667e829add', 'deleted profile picture', '234234234', '2024-09-28 08:49:19', NULL, 0),
-(2, '13a83d06-bbfe-4cfe-90a2-dd88ed14941a', 'updated profile picture', '234234234', '2024-09-28 08:49:28', NULL, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -170,10 +179,11 @@ CREATE TABLE `jspence_pushes` (
   `id` bigint(20) NOT NULL,
   `push_id` varchar(300) DEFAULT NULL,
   `push_daily` varchar(300) DEFAULT NULL,
-  `push_amount` double(10,2) NOT NULL,
+  `push_amount` double(10,2) DEFAULT NULL,
   `push_from` varchar(300) DEFAULT NULL,
   `push_to` varchar(300) DEFAULT NULL,
   `push_date` date DEFAULT NULL,
+  `push_on` enum('dialy','coffers') DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` datetime DEFAULT NULL ON UPDATE current_timestamp(),
   `push_status` tinyint(1) NOT NULL DEFAULT 0
@@ -223,6 +233,15 @@ ALTER TABLE `jspence_admin`
   ADD KEY `admin_last_login` (`admin_last_login`),
   ADD KEY `admin_email` (`admin_email`),
   ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `jspence_coffers`
+--
+ALTER TABLE `jspence_coffers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `coffer_id` (`coffers_id`),
+  ADD KEY `coffers_send_to` (`coffers_for`),
+  ADD KEY `createdAt` (`createdAt`);
 
 --
 -- Indexes for table `jspence_daily`
@@ -286,6 +305,12 @@ ALTER TABLE `jspence_admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
+-- AUTO_INCREMENT for table `jspence_coffers`
+--
+ALTER TABLE `jspence_coffers`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `jspence_daily`
 --
 ALTER TABLE `jspence_daily`
@@ -301,7 +326,7 @@ ALTER TABLE `jspence_denomination`
 -- AUTO_INCREMENT for table `jspence_logs`
 --
 ALTER TABLE `jspence_logs`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jspence_pushes`
