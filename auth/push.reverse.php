dc9a256ce714b6c33ec745e9b46654dd0130a8ca
morotijani <tjhackx111@gmail.com>
2024-10-15 03:56:08 +0000

    initial commit

--- auth/push.reverse.php
+++ auth/push.reverse.php
@@ -13,22 +13,25 @@
 				$pin = ((isset($_POST['admin_pin']) && !empty($_POST['admin_pin'])) ? sanitize($_POST['admin_pin']) : '');
 
 				// find push id
-
-				if ($pin == $admin_data['admin_pin']) {
-					$query = "
-						UPDATE jspence_pushes 
-						SET push_status = ? 
-						WHERE push_id = ?
-					";
-					$statement = $conn->prepare($query);
-					$result = $statement->execute([1, $id]);
-					if ($result) {
-						$_SESSION['flash_success'] = 'Push reversed successfully!';
+				$find = $conn->query("SELECT * FROM jspence_pushes WHERE push_id = '" . $id . "' LIMIT 1")->rowCount();
+				if ($find > 0) {
+					if ($pin == $admin_data['admin_pin']) {
+						$query = "
+							UPDATE jspence_pushes 
+							SET push_status = ? 
+							WHERE push_id = ?
+						";
+						$statement = $conn->prepare($query);
+						$result = $statement->execute([1, $id]);
+						if ($result) {
+							$_SESSION['flash_success'] = 'Push reversed successfully!';
+						}
+					} else {
+						$_SESSION['flash_error'] = 'Invalid admin pin provided!';
 					}
 				} else {
-					$_SESSION['flash_error'] = 'Invalid admin pin provided!';
+					$_SESSION['flash_error'] = 'Unknow push id provided!';
 				}
-
 			} else {
 				$_SESSION['flash_error'] = 'Provide admin pin to verify this reverse push!';
 			}
