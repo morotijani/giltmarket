b21c0f360a37de3b33876568826a1ba9668186f8
morotijani <tjhackx111@gmail.com>
2024-10-23 04:57:56 +0000

    initial commit

--- auth/push.reverse.php
+++ auth/push.reverse.php
@@ -24,43 +24,7 @@
 		// find push id
 		$find = $conn->query("SELECT * FROM jspence_pushes WHERE push_id = '" . $id . "' LIMIT 1")->fetchAll();
 		if (count($find) > 0) {
-			if (isset($_POST['admin_pin'])) {
-				if (!empty($_POST['admin_pin']) || $_POST['admin_pin'] != '') {
-
-					$reason = ((isset($_POST['reason']) && !empty($_POST['reason'])) ? sanitize($_POST['reason']) : '');
-					$pin = ((isset($_POST['admin_pin']) && !empty($_POST['admin_pin'])) ? sanitize($_POST['admin_pin']) : '');
-					if ($pin == $admin_data['admin_pin']) {
-						$query = "
-							UPDATE jspence_pushes 
-							SET push_status = ?, push_reverse_reason = ? 
-							WHERE push_id = ?
-						";
-						$statement = $conn->prepare($query);
-						$result = $statement->execute([1, $reason, $id]);
-						if ($result) {
-							if (admin_has_permission('salesperson')) {
-								$sql = "
-									UPDATE jspence_daily 
-									SET daily_balance = daily_balance + '" . $find[0]['push_amount'] . "'
-									WHERE daily_id = ? 
-								";
-								$statement = $conn->prepare($sql);
-								$statement->execute([$find[0]['push_daily']]);
-							}
-
-							$_SESSION['flash_success'] = 'Push reversed successfully!';
-							redirect(PROOT . 'account/pushes');
-						}
-					} else {
-						$_SESSION['flash_error'] = 'Invalid admin pin provided!';
-						redirect(PROOT . 'account/pushes');
-					}
-					
-				} else {
-					$_SESSION['flash_error'] = 'Provide admin pin to verify this reverse push!';
-					redirect(PROOT . 'account/pushes');
-				}
-			}
+			
 		} else {
 			$_SESSION['flash_error'] = 'Unknow push id provided!';
 			redirect(PROOT . 'account/pushes');
@@ -68,6 +32,63 @@
 	} else {
 		$_SESSION['flash_error'] = "What do you want?";
 		redirect(PROOT . 'account/pushes');
+	}
+
+	if (isset($_POST['admin_pin'])) {
+		if (!empty($_POST['admin_pin']) || $_POST['admin_pin'] != '') {
+
+			$reason = ((isset($_POST['reason']) && !empty($_POST['reason'])) ? sanitize($_POST['reason']) : '');
+			$pin = ((isset($_POST['admin_pin']) && !empty($_POST['admin_pin'])) ? sanitize($_POST['admin_pin']) : '');
+			if ($pin == $admin_data['admin_pin']) {
+
+				// check the balance of the person we are reversing from
+				if ($find[0]['push_to'] == 'coffers') {
+					$from_balance = _capital($find[0]['push_to'])['today_balance'];
+				} else {
+					if (admin_has_permission('salesperson')) {
+						$from_balance = _capital($find[0]['push_to'])['today_balance'];
+					} else if (admin_has_permission('supervisor')) {
+						$from_balance = remaining_gold_balance($find[0]['push_to']);
+					}
+				}
+				dnd($from_balance);
+
+				// incase the revesal amount is greater or equal to the remaining balance of the person we are reversing from, then we prevent reversal
+				if ($find[0]['push_amount'] < $from_balance) {
+					$query = "
+						UPDATE jspence_pushes 
+						SET push_status = ?, push_reverse_reason = ? 
+						WHERE push_id = ?
+					";
+					$statement = $conn->prepare($query);
+					$result = $statement->execute([1, $reason, $id]);
+					if ($result) {
+						if (admin_has_permission('salesperson')) {
+							$sql = "
+								UPDATE jspence_daily 
+								SET daily_balance = daily_balance + '" . $find[0]['push_amount'] . "'
+								WHERE daily_id = ? 
+							";
+							$statement = $conn->prepare($sql);
+							$statement->execute([$find[0]['push_daily']]);
+						}
+
+						$_SESSION['flash_success'] = 'Push reversed successfully!';
+						redirect(PROOT . 'account/pushes');
+					}
+				} else {
+					$_SESSION['flash_error'] = 'Reversal denied!';
+					redirect(PROOT . 'account/pushes');
+				}
+			} else {
+				$_SESSION['flash_error'] = 'Invalid admin pin provided!';
+				redirect(PROOT . 'account/pushes');
+			}
+			
+		} else {
+			$_SESSION['flash_error'] = 'Provide admin pin to verify this reverse push!';
+			redirect(PROOT . 'account/pushes');
+		}
 	}
 ?>
 	<div class="container-lg">
@@ -108,10 +129,10 @@
           	</div>
           	<div class="col-12 col-lg-9" data-bs-spy="scroll" data-bs-target="#accountNav" data-bs-smooth-scroll="true" tabindex="0">
 				<!-- General -->
-				<section class="card bg-body-tertiary border-transparent card-line mb-5" id="general">
+				<section class="card bg-body-tertiary border-transparent mb-5" id="general">
 					<div class="card-body">
 						<h2 class="fs-5 mb-1">Reverse <?= money($find[0]['push_amount']); ?></h2>
-						<p class="text-body-secondary">You are to reverse a push you made, provide pin to complete the reverse.</p>
+						<p class="text-body-secondary">You are to reverse a push you made, provide reason and pin to complete the reverse.</p>
 						<form id="reverseForm" method="POST">
 							<div class="mb-3">
 								<label class="form-label" for="fullName">Reason</label>
@@ -137,7 +158,7 @@
 		$('#submitReverse').attr('disabled', true);
 		$('#submitReverse').text('Reversing ...');
 		setTimeout(function () {
-			// $('#reverseForm').submit();
+			$('#reverseForm').submit();
 
 			$('#submitReverse').attr('disabled', false);
 			$('#submitReverse').text('Reverse');
