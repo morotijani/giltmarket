0e7d2df1da6a5c7d7700d2da9f61674f72d19472
morotijani <tjhackx111@gmail.com>
2024-11-08 08:03:51 +0000

    initial commit

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -250,6 +250,17 @@
         // 
         $(document).ready(function() {
 
+            function delay(callback, ms) {
+                var timer = 0;
+                return function() {
+                    var context = this, args = arguments;
+                    clearTimeout(timer);
+                    timer = setTimeout(function () {
+                    callback.apply(context, args);
+                    }, ms || 0);
+                };
+                }
+
             // Fade out messages
             $("#temporary").fadeOut(5000);
 
