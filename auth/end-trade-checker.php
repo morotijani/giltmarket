e7d536691abc9e84f517ea10a2ff3328febb2ade
morotijani <tjhackx111@gmail.com>
2024-11-10 05:10:45 +0000

    initial commit

--- auth/end-trade-checker.php
+++ auth/end-trade-checker.php
@@ -17,7 +17,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
@@ -43,8 +43,6 @@
     <!-- Title -->
     <title>JSPENCE</title>
 
-    <style>
-    </style>
 </head>
 
     <body class="d-flex align-items-center">
