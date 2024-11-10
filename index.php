f00332890a12f9ffa2bbbacbb0da6d234d846bfa
morotijani <tjhackx111@gmail.com>
2024-11-10 10:32:32 +0000

    intial commit

--- index.php
+++ index.php
@@ -97,12 +97,22 @@
 		<!-- Stats -->
 		<div class="row mb-8">
         	<div class="col-12 col-md-6 col-xxl-3 mb-4 mb-xxl-0">
-            	<div class="card bg-body-tertiary border-transparent">
+            	<div class="card bg-success-subtle border-transparent">
               		<div class="card-body">
                 		<div class="row align-items-center">
 							<div class="col">
 								<!-- Heading -->
-								<h4 class="fs-base fw-normal text-body-secondary mb-1"><?= ((admin_has_permission()) ? 'Today' : 'Capital'); ?></h4>
+								<h4 class="fs-base fw-normal text-body-secondary mb-1">
+									<?php 
+										if (admin_has_permission()) {
+											echo 'Today';
+										} else if (admin_has_permission('supervisor')) {
+											echo 'Gold given';
+										} else {
+											echo 'Money given';
+										}
+									?>
+								</h4>
 
 								<!-- Text -->
 								<div class="fs-5 fw-semibold">
@@ -123,7 +133,7 @@
             		</div>
           		</div>
           		<div class="col-12 col-md-6 col-xxl-3 mb-4 mb-xxl-0">
-					<div class="card bg-body-tertiary border-transparent">
+					<div class="card bg-warning-subtle border-transparent">
 						<div class="card-body">
 							<div class="row align-items-center">
 							<div class="col">
